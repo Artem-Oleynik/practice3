cb9b05aeda9b7d65874067e761505c1a504f3bc1
Artem-Oleynik <m0999308739@gmail.com>
2018-11-28 08:03:56 +0200

    to do

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 var list_users = [];
 
 function addNewUser(){
-	var first_name = document.getElementsByName('input_first_name').value;
-	var last_name = document.getElementsByName('input_last_name').value;
-	var age = document.getElementsByName('input_age').value;
-	var city = document.getElementsByName('input_city').value;
-	var interests = document.getElementsByName('list_interests').options[document.getElementsByName('list_interests').selectedIndex].value;
-	var inputinterests = document.getElementsByName('input_interests').value;
+	var first_name = document.getElementById('input_first_name').value;
+	var last_name = document.getElementById('input_last_name').value;
+	var age = document.getElementById('input_age').value;
+	var city = document.getElementById('input_city').value;
+	var interests = document.getElementById('list_interests').options[document.getElementById('list_interests').selectedIndex].value;
+	var inputinterests = document.getElementById('input_interests').value;
 
-	window.list_users[window.list_users.length] = new User(first_name, last_name, age, city, getinterents(interests, inputinterests));
+	window.list_users[window.list_users.length] = new User(first_name, last_name, age, city, interests, inputinterests);
 	checkonAsync();
 }
 
 //проверка на асинхронизацию с html разметкой
 function checkonAsync(){
 	var countUsers = document.getElementsByTagName('tr').length;
-	if((countUsers-1) == window.list_users.length){
-		document.getElementByTagName('table').innerHTML += '<tr><td>' + User.first_name +' '+ User.last_name + '</td><td>'+ User.age +'</td><td>Gotham</td><td>' + User.interests + checkonValue(User.inputinterests) + '</td><td><button>x</button></td></tr>';
-	}
+	var indexLast = window.list_users.length-1;
+	// if((countUsers-1) == window.list_users.length){
+	document.getElementById('listhead').innerHTML += '<tr><td>' + window.list_users[indexLast].first_name +' '+ window.list_users[indexLast].last_name + '</td><td>'+ window.list_users[indexLast].age +'</td><td>Gotham</td><td>' + window.list_users[indexLast].interests + checkonValue(window.list_users[indexLast].inputinterests) + '</td><td><button>x</button></td></tr>';
+	//'
+	// alert(indexLast);
+	// }
 }
 
 function checkonValue(value){
